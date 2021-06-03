f17b728e501792d71ea99910704e9e9fbaf9fc8a
SITANSHU SEKHAR MISHRA <18cse022@gmail.com>
2021-06-03 19:49:58 +0530

    Add files via upload

---
+++ coloured-text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Sitanshu Sekhar Mishra "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Mishra")
+print(Fore.RED + Back.GREEN+ "Hi My name is Sitanshu")
